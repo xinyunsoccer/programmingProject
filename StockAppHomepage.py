3fb0bd8dbe7d22e6d21f426d7259c568de2e1cb7
YungRangeRover99 <114655075+YungRangeRover99@users.noreply.github.com>
2023-05-18 18:28:50 +0200

    Update StockAppHomepage.py

--- StockAppHomepage.py
+++ StockAppHomepage.py
@@ -5,9 +5,10 @@ Created on Fri May 12 17:24:33 2023
 
 @author: lucas
 """
-
+# import streamlit library 
 import streamlit as st 
 
+# set up the page configuration 
 st.set_page_config(
     page_title='🚀📈 Stock Analytics and Portfolio App 📈🚀',
     page_icon="📈", 
@@ -15,13 +16,14 @@ st.set_page_config(
     layout = 'wide'
     )
 
+# set the Title for our App
 st.title(' 🚀📈 Stock Analytics and Portfolio App 📈🚀')
 
-#Create a more in-depth description later
-
+# add a Description for the App 
 st.subheader('Welcome to our Programming Project Page') 
 st.write("""We have created a page with which you can make more informed investment decisions. (1) First of all, you can analyse the past performance and KPIs of a stock of your choice and predict its future price.
  (2) On the second page you can create a portfolio of your choice based on the stocks you have analysed before and manually decide how much you weigh each stock. Based on this you can see the performance of your created portfolio. 
  (3) Lastly, you can go to the Stock Portfolio Optimizer where you can input the stocks you picked for your portfolio and check how the implemented portfolio Optimizer would distribute the weights of your selected portfolio. Have fun!""")
 
+# Display the indicator to select a page
 st.sidebar.success('Select a page above.')
