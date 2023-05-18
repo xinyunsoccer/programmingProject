0684538cc6381c5bb086d9ecb46ad25e36d6120a
YungRangeRover99 <114655075+YungRangeRover99@users.noreply.github.com>
2023-05-18 18:25:58 +0200

    Update StockAppHomepage.py

--- StockAppHomepage.py
+++ StockAppHomepage.py
@@ -20,8 +20,8 @@ st.title(' 🚀📈 Stock Analytics and Portfolio App 📈🚀')
 #Create a more in-depth description later
 
 st.subheader('Welcome to our Programming Project Page') 
-st.write("""We have created a page with which you can make more informed investment decisions. First of all, you can analyse the past performance and KPIs of a stock of your choice and predict its future price.
-On the second page you can create a portfolio of your choice based on the stocks you have analysed before and manually decide how much you weigh each stock. Based on this you can see the performance of your created portfolio. 
-Lastly, you can go to the Stock Portfolio Optimizer where you can input the stocks you picked for your portfolio and check how the implemented portfolio Optimizer would distribute the weights of your selected portfolio. Have fun!""")
+st.write("""We have created a page with which you can make more informed investment decisions. (1) First of all, you can analyse the past performance and KPIs of a stock of your choice and predict its future price.
+ (2) On the second page you can create a portfolio of your choice based on the stocks you have analysed before and manually decide how much you weigh each stock. Based on this you can see the performance of your created portfolio. 
+ (3) Lastly, you can go to the Stock Portfolio Optimizer where you can input the stocks you picked for your portfolio and check how the implemented portfolio Optimizer would distribute the weights of your selected portfolio. Have fun!""")
 
 st.sidebar.success('Select a page above.')
