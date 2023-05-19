cdfdc565c63d4659bebf8191e817c3a7ce165e00
PKS97 <133778183+PKS97@users.noreply.github.com>
2023-05-19 10:17:22 +0200

    Update StockAppHomepage.py

--- StockAppHomepage.py
+++ StockAppHomepage.py
@@ -19,11 +19,13 @@ st.set_page_config(
 # set the Title for our App
 st.title(' 🚀📈 Stock Analytics and Portfolio App 📈🚀')
 
-# add a Description for the App 
+# add a Description for the App with bullet point enumeration
+st.subheader('')
 st.subheader('Welcome to our Programming Project Page') 
-st.write("""We have created a page with which you can make more informed investment decisions. (1) First of all, you can analyse the past performance and KPIs of a stock of your choice and predict its future price.
- (2) On the second page you can create a portfolio of your choice based on the stocks you have analysed before and manually decide how much you weigh each stock. Based on this you can see the performance of your created portfolio. 
- (3) Lastly, you can go to the Stock Portfolio Optimizer where you can input the stocks you picked for your portfolio and check how the implemented portfolio Optimizer would distribute the weights of your selected portfolio. Have fun!""")
+st.write('We have created a page with which you can make more informed investment decisions.')
+st.markdown('- First of all, you can analyse the past performance and KPIs of a stock of your choice and predict its future price.')
+st.markdown('- On the second page you can create a portfolio of your choice based on the stocks you have analysed before and manually decide how much you weigh each stock. Based on this you can see the performance of your created portfolio.') 
+st.markdown('- Lastly, you can go to the Stock Portfolio Optimizer where you can input the stocks you picked for your portfolio and check how the implemented portfolio Optimizer would distribute the weights of your selected portfolio. Have fun!')
 
 # Display the indicator to select a page
 st.sidebar.success('Select a page above.')
